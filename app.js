d5f37d3e58291dd63ea399f638744d6227254cb8
Ilya Yurchenkov <ilya_su@intekart.ru>
2021-07-03 22:30:17 +0300

    feat profile edit

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const errorsMiddleware = require('./middlewares/errors');
 
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3000, MONGO_URL = 'mongodb://localhost:27017/bitfilmsdb' } = process.env;
+const { PORT = 3050, MONGO_URL = 'mongodb://localhost:27017/bitfilmsdb' } = process.env;
 const app = express();
 
 mongoose.connect(MONGO_URL, {
@@ -20,7 +20,26 @@ mongoose.connect(MONGO_URL, {
   useUnifiedTopology: true,
 });
 
-app.use(cors());
+var whitelist = [
+  "http://localhost:3001",
+  "https://urchrr-mesto.nomoredomains.icu"
+];
+
+var corsOptionsDelegate = function(req, callback) {
+  console.log('req: ', req);
+  var corsOptions;
+  if (whitelist.indexOf(req) !== -1) {
+    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false }; // disable CORS for this request
+  }
+  console.log("!", corsOptions);
+  callback(null, corsOptions); // callback expects two parameters: error and options
+};
+
+app.use(
+  cors({ origin: corsOptionsDelegate, exposedHeaders: "*", credentials: true })
+);
 app.use(helmet());
 app.use(requestLogger);
 app.use(limiter);
